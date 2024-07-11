6e7cd3ff644936b927aa32ac3677e52b05c8a188
Takaharu Kobayashi <takaharu_kobayashi@yahoo.com>
2024-07-12 07:44:22 +0900

    correct vite.config.ts again.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES
-  ? "conoha-sample"
-  : "/",
+  base: process.env.GITHUB_PAGES ? "/conoha-sample/" : "/",
   plugins: [react()],
   server: {
     proxy: {
