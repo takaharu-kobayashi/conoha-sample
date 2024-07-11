214e3f267383b8d4cc442ea4e61efae03f799424
Takaharu Kobayashi <takaharu_kobayashi@yahoo.com>
2024-07-12 08:15:09 +0900

    re-build again.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "/conoha-sample/" : "/",
+  base: process.env.GITHUB_PAGES ? "/conoha-sample/" : "./",
   plugins: [react()],
   server: {
     proxy: {
